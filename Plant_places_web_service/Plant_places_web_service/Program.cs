dc80a4aa1a58bf86d8817131fd6ed8577fc17191
khalid.cuet <khalid.cuet@gmail.com>
2017-11-30 15:18:04 -0500

    commit1

---
+++ Plant_places_web_service/Plant_places_web_service/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plant_places_web_service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HI!!I am checking whether my git hub connection with visual studio is working");
+        }
+    }
+}
